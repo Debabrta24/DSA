d58a903bcbbe6dfefdd3921661c25026102f8a56
Debabrata24 <mr.debabrtapc2006@gmail.com>
2026-05-21 23:02:42 +0530

    dsa

--- leetcode/1780.check-if-number-is-a-sum-of-powers-of-three.cpp
+++ leetcode/1780.check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -8,35 +8,49 @@
 class Solution
 {
 public:
+    // ///this functio is for approach 3 👇👇👇👇👇👇👇
+    int solve(int n, int p)
+    {
+        if (n == 0)
+        {
+            return true;
+        }
+        if (pow(3, p) > n)
+        {
+            return false;
+        }
+        bool p_ko_lo = solve(n - pow(3, p), p + 1);
+        bool p_ko_mat_lo = solve(n, p + 1);
+        return p_ko_lo || p_ko_mat_lo;
+    }
+
     bool checkPowersOfThree(int n)
     {
-        // //aproach1
-        // int p = 0;
-        // while (pow(3, p) <= n)
-        // {
+        // // aproact 1
+        // int p=0 ;
+        // while (pow(3,p)<=n){
         //     p++;
         // }
-        // while (n > 0)
-        // {
-        //     if (n >= pow(3, p))
-        //     {
-        //         n = n - pow(3, p);
+        // while (n>0){
+        //     if(n>=pow(3,p)){
+        //         n=n-pow(3,p);
         //     }
-        //     if (n >= pow(3, p))
-        //     {
-        //         return false;
+        //     if(n>=pow(3,p)){
+        //        return false ;
         //     }
         //     p--;
         // }
         // return true;
-         // approach 2
-        while (n>0){
-            if(n%3==2){
-                return false;
-            }
-            n=n/3;
-        }
-        return true;
+        // //// approach 2
+        // while (n>0){
+        //     if(n%3==2){
+        //         return false;
+        //     }
+        //     n=n/3;
+        // }
+        // return true;
+        ///////// aproach 3
+        return solve(n, 0);
     }
 };
 // @lc code=end
